091e1984f2111b485bc5751ab73afc315568c169
Boris Vinogradov <no111u3@gmail.com>
2012-06-21 12:49:58 +0700

    create work.(c,h), modify startup.c for run work()

--- 01-gpio/00-base/startup.c
+++ 01-gpio/00-base/startup.c
@@ -2,17 +2,20 @@
 * Author: Boris Vinogradov(nis) : no111u3@gmail.com
 * File: startup.c
 * Type: C code file
-* Project: f4examples/00-base
+* Project: f4examples/01-gpio/00-base
 * 2012
 *-----------------------------------------------------------------------------*/
 /* include headers */
 #include "stm32f4xx.h"
 /* external vars */
 extern unsigned long _estack;
+extern void work;
 /* prototypes of functions */
 void reset_hander(void);
 /* reset handler */
 void reset_handler(void) {
+    /* call working code */
+    work();
 }
 /* table of Cortex vectors */
 void *vector_table[] __attribute__ ((section(".vectors"))) = {
---
+++ 01-gpio/00-base/work.c
@@ -0,0 +1,13 @@
+/*-----------------------------------------------------------------------------
+* Author: Boris Vinogradov(nis) : no111u3@gmail.com
+* File: work.c
+* Type: C code file
+* Project: f4examples/01-gpio/00-base
+* 2012
+*-----------------------------------------------------------------------------*/
+/* include headers */
+#include "stm32f4xx.h"
+#include "work.h"
+/* work */
+void work() {
+}
