3436a15d216897c90a405c397f21f9ce91be4fb6
Boris Vinogradov <no111u3@gmail.com>
2012-06-27 10:56:19 +0700

    Final project 01-gpio/01-led_en

---
+++ 01-gpio/01-led_en/work.c
@@ -0,0 +1,23 @@
+/*-----------------------------------------------------------------------------
+* Author: Boris Vinogradov(nis) : no111u3@gmail.com
+* File: work.c
+* Type: C code file
+* Project: f4examples/01-gpio/00-base
+* 2012
+*-----------------------------------------------------------------------------*/
+/* include headers */
+#include "stm32f4xx.h"
+#include "work.h"
+/* work */
+void work() {
+    /* enable led port */
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+    /* set led pins to output state */
+    LED_PORT->MODER |=
+        GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0 |
+        GPIO_MODER_MODER14_0 | GPIO_MODER_MODER15_0;
+    /* enable leds */
+    LED_PORT->ODR |= ALL_LEDS;
+    /* infinity loop */
+    while (1);
+}
