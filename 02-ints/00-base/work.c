11957b1c7dccc761cabe520fa45ddce6001380b1
Boris Vinogradov <no111u3@gmail.com>
2012-07-22 23:33:55 +0700

    Final project 02-ints/00-base

---
+++ 02-ints/00-base/work.c
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------------------
+* Author: Boris Vinogradov(nis) : no111u3@gmail.com
+* File: work.c
+* Type: C code file
+* Project: f4examples/01-gpio/00-base
+* 2012
+*-----------------------------------------------------------------------------*/
+/* include headers */
+#include "stm32f4xx.h"
+#include "work.h"
+/* work */
+void work() {
+    /* infinity loop */
+    while (1);
+}
