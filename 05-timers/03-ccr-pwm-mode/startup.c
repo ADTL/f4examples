8181307e2f56e610b19573c5a5a68c4327c1693b
Boris Vinogradov <no111u3@gmail.com>
2012-07-26 18:20:48 +0700

    Final project 05-timers/03-ccr-pwm-mode

---
+++ 05-timers/03-ccr-pwm-mode/startup.c
@@ -0,0 +1,155 @@
+/*-----------------------------------------------------------------------------
+* Author: Boris Vinogradov(nis) : no111u3@gmail.com
+* File: startup.c
+* Type: C code file
+* Project: f4examples/01-gpio/00-base
+* 2012
+*-----------------------------------------------------------------------------*/
+/* include headers */
+#include "stm32f4xx.h"
+#include "work.h"
+/* external vars */
+extern unsigned long _estack;
+extern void work();
+/* prototypes of functions */
+void reset_hander(void);
+/* reset handler */
+void reset_handler(void) {
+    /* call working code */
+    work();
+}
+/* nmi handler */
+void nmi_handler(void) {
+    while (1);
+}
+/* hardware fault handler */
+void hard_fault_handler(void) {
+    while (1);
+}
+/* memory management handler */
+void mem_manage_handler(void) {
+    while (1);
+}
+/* bus fault handler */
+void bus_fault_handler(void) {
+    while (1);
+}
+/* usage fault handler */
+void usage_fault_handler(void) {
+    while (1);
+}
+/* exti0 irq handler */
+void exti0_irq_handler(void) {
+    if (EXTI->PR | EXTI_PR_PR0) {
+        /* change pwm submode for tim4 ccch2,4 */
+        TIM4->CCMR1 ^= TIM_CCMR1_OC2M_0;
+        TIM4->CCMR2 ^= TIM_CCMR2_OC4M_0;
+        /* clear pending irq */
+        EXTI->PR |= EXTI_PR_PR0;
+        /* disable exti0 interrupt */
+        EXTI->IMR &= ~EXTI_IMR_MR0;
+    }
+}
+/* table of Cortex vectors */
+void *vector_table[] __attribute__ ((section(".vectors"))) = {
+    &_estack,           /*! stack begin */
+    /* Core interrupts */
+    reset_handler,      /*!#0 cortex-m4 reset interrupt begin code of this */
+    nmi_handler,        /*!#1 cortex-m4 non maskable interrupt */
+    hard_fault_handler, /*!#2 cortex-m4 hardware fault interrupt */
+    mem_manage_handler, /*!#3 cortex-m4 memory management interrupt */
+    bus_fault_handler,  /*!#4 cortex-m4 bus fault interrupt */
+    usage_fault_handler,/*!#5 cortex-m4 usage fault interrupt */
+    0,                  /*!#6 reserved */
+    0,                  /*!#7 reserved */
+    0,                  /*!#8 reserved */
+    0,                  /*!#9 reserved */
+    0,                  /*!#10 cortex-m4 system service interrupt */
+    0,                  /*!#11 cortex-m4 debug monitor interrupt */
+    0,                  /*!#12 reserved */
+    0,                  /*!#13 cortex-m4 penable request for system service interrupt */
+    0,                  /*!#14 cortex-m4 system tick timer interrupt */
+    /* External Interrupts */
+    0,                  /*!%0 Window WatchDog              */                                        
+    0,                  /*!%1 PVD through EXTI Line detection */                        
+    0,                  /*!%2 Tamper and TimeStamps through the EXTI line */            
+    0,                  /*!%3 RTC Wakeup through the EXTI line */                      
+    0,                  /*!%4 FLASH                        */                                          
+    0,                  /*!%5 RCC                          */                                            
+    exti0_irq_handler,  /*!%6 EXTI Line0                   */                        
+    0,                  /*!%7 EXTI Line1                   */                          
+    0,                  /*!%8 EXTI Line2                   */                          
+    0,                  /*!%9 EXTI Line3                   */                          
+    0,                  /*!%10 EXTI Line4                   */                          
+    0,                  /*!%11 DMA1 Stream 0                */                  
+    0,                  /*!%12 DMA1 Stream 1                */                   
+    0,                  /*!%13 DMA1 Stream 2                */                   
+    0,                  /*!%14 DMA1 Stream 3                */                   
+    0,                  /*!%15 DMA1 Stream 4                */                   
+    0,                  /*!%16 DMA1 Stream 5                */                   
+    0,                  /*!%17 DMA1 Stream 6                */                   
+    0,                  /*!%18 ADC1, ADC2 and ADC3s         */                   
+    0,                  /*!%19 CAN1 TX                      */                         
+    0,                  /*!%20 CAN1 RX0                     */                          
+    0,                  /*!%21 CAN1 RX1                     */                          
+    0,                  /*!%22 CAN1 SCE                     */                          
+    0,                  /*!%23 External Line[9:5]s          */                          
+    0,                  /*!%24 TIM1 Break and TIM9          */         
+    0,                  /*!%25 TIM1 Update and TIM10        */         
+    0,                  /*!%26 TIM1 Trigger and Commutation and TIM11 */
+    0,                  /*!%27 TIM1 Capture Compare         */                          
+    0,                  /*!%28 TIM2                         */                   
+    0,                  /*!%29 TIM3                         */                   
+    0,                  /*!%30 TIM4                         */                   
+    0,                  /*!%31 I2C1 Event                   */                          
+    0,                  /*!%32 I2C1 Error                   */                          
+    0,                  /*!%33 I2C2 Event                   */                          
+    0,                  /*!%34 I2C2 Error                   */                            
+    0,                  /*!%35 SPI1                         */                   
+    0,                  /*!%36 SPI2                         */                   
+    0,                  /*!%37 USART1                       */                   
+    0,                  /*!%38 USART2                       */                   
+    0,                  /*!%39 USART3                       */                   
+    0,                  /*!%40 External Line[15:10]s        */                          
+    0,                  /*!%41 RTC Alarm (A and B) through EXTI Line */                 
+    0,                  /*!%42 USB OTG FS Wakeup through EXTI line */                       
+    0,                  /*!%43 TIM8 Break and TIM12         */         
+    0,                  /*!%44 TIM8 Update and TIM13        */         
+    0,                  /*!%45 TIM8 Trigger and Commutation and TIM14 */
+    0,                  /*!%46 TIM8 Capture Compare         */                          
+    0,                  /*!%47 DMA1 Stream7                 */                          
+    0,                  /*!%48 FSMC                         */                   
+    0,                  /*!%49 SDIO                         */                   
+    0,                  /*!%50 TIM5                         */                   
+    0,                  /*!%51 SPI3                         */                   
+    0,                  /*!%52 UART4                        */                   
+    0,                  /*!%53 UART5                        */                   
+    0,                  /*!%54 TIM6 and DAC1&2 underrun errors */                   
+    0,                  /*!%55 TIM7                         */
+    0,                  /*!%56 DMA2 Stream 0                */                   
+    0,                  /*!%57 DMA2 Stream 1                */                   
+    0,                  /*!%58 DMA2 Stream 2                */                   
+    0,                  /*!%59 DMA2 Stream 3                */                   
+    0,                  /*!%60 DMA2 Stream 4                */                   
+    0,                  /*!%61 Ethernet                     */                   
+    0,                  /*!%62 Ethernet Wakeup through EXTI line */                     
+    0,                  /*!%63 CAN2 TX                      */                          
+    0,                  /*!%64 CAN2 RX0                     */                          
+    0,                  /*!%65 CAN2 RX1                     */                          
+    0,                  /*!%66 CAN2 SCE                     */                          
+    0,                  /*!%67 USB OTG FS                   */                   
+    0,                  /*!%68 DMA2 Stream 5                */                   
+    0,                  /*!%69 DMA2 Stream 6                */                   
+    0,                  /*!%70 DMA2 Stream 7                */                   
+    0,                  /*!%71 USART6                       */                    
+    0,                  /*!%72 I2C3 event                   */                          
+    0,                  /*!%73 I2C3 error                   */                          
+    0,                  /*!%74 USB OTG HS End Point 1 Out   */                   
+    0,                  /*!%75 USB OTG HS End Point 1 In    */                   
+    0,                  /*!%76 USB OTG HS Wakeup through EXTI */                         
+    0,                  /*!%77 USB OTG HS                   */                   
+    0,                  /*!%78 DCMI                         */                   
+    0,                  /*!%79 CRYP crypto                  */                   
+    0,                  /*!%80 Hash and Rng                 */
+    0,                  /*!%81 FPU                          */
+};
--- 05-timers/03-ccr-pwm-mode/work.c
+++ 05-timers/03-ccr-pwm-mode/work.c
@@ -13,7 +13,7 @@
 /* work */
 void work() {
     /* enable led & button port */
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOAEN;
     /* enable tim4 */
     RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
     /* set led pins to af state */
@@ -23,6 +23,14 @@ void work() {
     /* select af block - tim3..5(af2 function) to led pins */
     LED_PORT->AFR[1] |= (GPIO_AF2_TIM3_5 << 28) | (GPIO_AF2_TIM3_5 << 24) |
         (GPIO_AF2_TIM3_5 << 20) | (GPIO_AF2_TIM3_5 << 16);
+    /* pull-up button io */
+    BUT_PORT->PUPDR |= GPIO_PUPDR_PUPDR0_1;
+    /* route exti0 to pa0 */
+    SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA;
+    /* setup exti0 to failing edge */
+    EXTI->FTSR |= EXTI_FTSR_TR0;
+    /* enable exti0 interrupt */
+    EXTI->IMR |= EXTI_IMR_MR0;
     /* tim4 setup presc */
     TIM4->PSC = 16000 - 1;
     /* tim4 setup preload reg */
@@ -33,12 +41,17 @@ void work() {
     TIM4->CCR3 = 500;
     TIM4->CCR4 = 500;
     /* tim4 setup cc chanel1..4 */
-    TIM4->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC2M_0 | TIM_CCMR1_OC2M_1;
-    TIM4->CCMR2 |= TIM_CCMR2_OC3M_0 | TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC4M_0 | TIM_CCMR2_OC4M_1;
+    TIM4->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2;
+    TIM4->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_2;
     /* tim4 setup cc chanel1..4 to corresponding output pin */
     TIM4->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E | TIM_CCER_CC4E;
     /* tim4 enable clock */
     TIM4->CR1 |= TIM_CR1_CEN;
+    /* nvic enable exti0 irq */
+    NVIC_EnableIRQ(EXTI0_IRQn);
     /* infinity loop */
-    while (1);
+    while (1) {
+        /* enable exti0 interrupt */
+        EXTI->IMR |= EXTI_IMR_MR0;
+    }
 }
